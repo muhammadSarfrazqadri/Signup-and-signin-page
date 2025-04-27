6b712dfe4b4f4a0bd3f58d0016f15f5c27996f49
Muhammad Sarfraz <msowner.best@gmail.com>
2025-04-28 02:35:29 +0500

    com

--- app.js
+++ app.js
@@ -1,38 +1,39 @@
-// function signUpPage() {
-//   document.getElementById("signup-form").style.display = "flex";
-//   document.getElementById("login-form").style.display = "none";
-// }
+function signUpPage() {
+  document.getElementById("signup-form").style.display = "flex";
+  document.getElementById("login-form").style.display = "none";
+}
 
-// function signInPage() {
-//     document.getElementById("login-error").innerText = "";
-//     document.getElementById("login-username").value = "";
-//     document.getElementById("login-password").value = "";
-//   document.getElementById("login-form").style.display = "flex";
-//   document.getElementById("signup-form").style.display = "none";
-// }
+function signInPage() {
+    document.getElementById("login-error").innerText = "";
+    document.getElementById("login-username").value = "";
+    document.getElementById("login-password").value = "";
+  document.getElementById("login-form").style.display = "flex";
+  document.getElementById("signup-form").style.display = "none";
+}
 
-// let users = {};
+let users = {};
 
-// function register() {
-//   let userName = document.getElementById("signup-username").value;
-//   let password = document.getElementById("signup-password").value;
+function register() {
+  let userName = document.getElementById("signup-username").value;
+  let password = document.getElementById("signup-password").value;
 
-//   users[userName] = password;
-//   signInPage();
-//   document.getElementById("signup-username").value = "";
-//   document.getElementById("signup-password").value = "";
-// }
+  users[userName] = password;
+  signInPage();
+  document.getElementById("signup-username").value = "";
+  document.getElementById("signup-password").value = "";
+}
 
-// function login() {
-//   let userName = document.getElementById("login-username").value;
-//   let password = document.getElementById("login-password").value;
+function login() {
+  let userName = document.getElementById("login-username").value;
+  let password = document.getElementById("login-password").value;
 
-//   if (users[userName] && users[userName] === password) {
-//     document.getElementById('welcome').style.display = 'block'
-//     document.getElementById('login-form').style.display = 'none'
-//     document.getElementById('signup-form').style.display = 'none'
-//     document.getElementById('love-audio').play()
-//   } else {
-//     document.getElementById("login-error").innerText = "Invalid Name Or Password";
-//   }
-// }
+  if (users[userName] && users[userName] === password) {
+    document.getElementById('welcome').style.display = 'block'
+    document.getElementById('login-form').style.display = 'none'
+    document.getElementById('signup-form').style.display = 'none'
+    document.getElementById('container').style.display = 'none'
+    document.getElementById('love-audio').play()
+  } else {
+    document.getElementById("login-error").innerText = "Invalid Name Or Password";
+  }
+}
